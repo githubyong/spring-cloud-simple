f56c88bceb5b071a60e807d4ac83c302eced419b
yong <12374011@163.com>
2022-04-09 19:10:22 +0800

    refact folder

--- scs-withsecurity/scs-withsecurity-auth/src/main/java/org/example/auth/dao/UserRepository.java
+++ scs-withsecurity/scs-withsecurity-auth/src/main/java/org/example/auth/dao/UserRepository.java
@@ -3,6 +3,7 @@ package org.example.auth.dao;
 import org.example.auth.model.ScsUser;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,5 +11,5 @@ public interface UserRepository extends CrudRepository<ScsUser,Integer> {
 
 
     @Query("select * from user where username=:name")
-    List<ScsUser> findByUsername(String name);
+    List<ScsUser> findByUsername(@Param("name") String name);
 }
